1201330dc02b4522156d980c98812c50eb37effa
flopezalvaradoo <202211154@alu.comillas.edu>
2024-09-30 22:46:03 +0200

    Se sube el proyecto

---
+++ PoliceStation.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Practice2
+{
+    class PoliceStation
+    {
+        private const string typeOfVehicle = "Police Car";
+        private bool isPatrolling;
+        private SpeedRadar speedRadar;
+
+        public PoliceStation(string plate) : base(typeOfVehicle, plate)
+        {
+            isPatrolling = false;
+            speedRadar = new SpeedRadar();
+        }
+
+        public void UseR()
+        {
+
+        }
+    }
+}
