d7a22088c5a48b164421965e7bfba69d49d0bba9
flopezalvaradoo <202211154@alu.comillas.edu>
2024-10-02 09:48:06 +0200

    Se actualiza la clase PoliceStation (Parte1: apartado1)

--- PoliceStation.cs
+++ PoliceStation.cs
@@ -8,19 +8,46 @@ namespace Practice2
 {
     class PoliceStation
     {
-        private const string typeOfVehicle = "Police Car";
-        private bool isPatrolling;
-        private SpeedRadar speedRadar;
+        private bool alarm;
+        private string plateOffender;
+        public List<PoliceCar> policeCarList { get; private set; }
 
-        public PoliceStation(string plate) : base(typeOfVehicle, plate)
+        public PoliceStation()
         {
-            isPatrolling = false;
-            speedRadar = new SpeedRadar();
+            alarm = false;
+            policeCarList = new List<PoliceCar>();
+            plateOffender = string.Empty;
         }
 
-        public void UseR()
+        public void AddPoliceCarToStation(string plate)
         {
+            PoliceCar police1 = new PoliceCar(plate);
+            policeCarList.Add(police1);
+        }
 
+        public void ActivateAlarm()
+        {
+            if (alarm == false)
+            {
+                alarm = true;
+            }
+        }
+        public void DeactivateAlarm()
+        {
+            if (alarm == true)
+            {
+                alarm = false;
+            }
+        }
+
+        public void SetPlateOffender(string plate)
+        {
+            plateOffender = plate;
+        }
+
+        public void ChaseCar()
+        {
+//hacer que los coches se pongan en modo persecucion
         }
     }
 }
